e493e59f8f1369ab5b40bc34ee1636c44485ad09
Sam <sam@faitaccomp.li>
2021-03-21 11:23:05 -0400

    Add Sitemaps

--- workers-site/index.js
+++ workers-site/index.js
@@ -23,7 +23,10 @@ async function handleRequest(event) {
 async function handleUncached(event){
   const keys = new URL(event.request.url).host.split('.').slice(0,2)
   if (event.request.url.endsWith('.png')){return await getAssetFromKV(event, {})}
-  if (keys[0] === 'what'){return await listPage(event);}
+  else if (event.request.url.endsWith('robots.txt')){return new Response('Sitemap: http://what.iscalled.com/sitemap.txt', {})}
+  else if (event.request.url.endsWith('sitemap.txt')){return await sitemapText()}
+  else if (event.request.url.endsWith('sitemap.xml')){return await sitemapXml()}
+  else if (keys[0] === 'what'){return await listPage(event);}
   else {return await infoPage(event, keys);}
 }
 
@@ -40,7 +43,7 @@ async function listPage(event){
 }
 
 async function infoPage(event, keys){
-  const data = JSON.parse(await CALLED.get(keys.join('.')))
+  const data = JSON.parse(await CALLED.get(keys.join('.').replace('-', ' ')))
   const page = await getAssetFromKV(event, {})
   const typeString = getTypeString(keys[1], keys[0])
   return new HTMLRewriter()
@@ -64,6 +67,31 @@ function getTypeString(type, animal){
     case 'infant': return `An Infant ${animalCap} is called`
     default: return `I don't know anything about ${animalString} ${type}`
   }
+}
+
+async function sitemapText(){
+  let names = JSON.parse(await CALLED.get('animals'));
+  let text = "http://what.iscalled.com\n"
+  for (let name of names){
+    for (let type of ['group', 'meat', 'female', 'male', 'infant']){
+      text += `http://${name.replace(' ', '-')}.${type}.iscalled.com\n`
+    }
+  }
+  return new Response(text, {})
+}
+
+async function sitemapXml(){
+  let names = JSON.parse(await CALLED.get('animals'));
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`
+  xml += `<url>\n\t<loc>http://what.iscalled.com</loc>\n</url>\n`
+  for (let name of names){
+    for (let type of ['group', 'meat', 'female', 'male', 'infant']){
+      xml += `<url>\n\t<loc>http://${name.replace(' ', '-')}.${type}.iscalled.com</loc>\n</url>\n`
+    }
+  }
+
+  xml += '</urlset>'
+  return new Response(xml, {});
 }
 
 class TitleElementHandler {
