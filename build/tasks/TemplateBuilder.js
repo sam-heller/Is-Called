fc25db810869a6db1c400e23788a24340b2894ca
Sam <sam@faitaccomp.li>
2021-03-23 19:58:01 -0400

    Initital stab at adding additional image sources

--- build/tasks/TemplateBuilder.js
+++ build/tasks/TemplateBuilder.js
@@ -5,7 +5,7 @@ const minify = require('html-minifier').minify
 const CloudflareAPI = require('../../lib/CloudflareAPI')
 require('../../lib/over-engineering')
 class TemplateBuilder {
-    unsplash_available = true
+    unsplash_available = false
     
     constructor(){
         nunjucks.configure(process.cwd() + '/build/templates', {autoescape: false})
@@ -22,24 +22,25 @@ class TemplateBuilder {
         let pages = []
         for (let entry of data){
             let values = {animal: Object.keys(entry.animal)[0],tags: []}
-            values.img = await(this.getImage(values.animal))
+            values.images = await(this.getImages(values.animal))
             values.plural = values.animal.pluralize()
             values.connector = values.animal.length == values.plural.length ? 'are' : 'is'
             for  (let type of ['group', 'male', 'female', 'infant', 'meat']){
                 values.tags = [];
                 for(let group of Object.keys(entry[type])){
                     values.tags.push({name: group, sub: entry[type][group]})
-                }                                     
+                }
+                values.type = type                                     
                 let infoCard = await this.renderInfoCard(entry, values, type)
                 let infoPage = await this.renderPage({content: infoCard, entry: entry})
                 if (cloudflare_out){pages.append({key: `${values.animal}.${type}`, page: infoPage})}
                 if (file_out){
-                     fs.writeFileSync(process.cwd() + `/public/detail-pages/${values.animal}.${type}.html`, infoPage)
+                     await fs.writeFileSync(process.cwd() + `/public/detail-pages/${values.animal.replace(' ', '-')}.${type}.html`, infoPage)
                 }
-                // console.log(infoPage);
             }
         }
     }
+
 
     async renderPage(data, minify=false){
         try {
@@ -68,20 +69,25 @@ class TemplateBuilder {
         
     }
 
-    async getImage(animal){
+    async getImages(animal, first_run=true){
         let images = await this.cloudflare.getValue(`${animal}.images`, 'data', []);
         if (images.length == 0 && this.unsplash_available){
-            if (this.unsplash_available){
-                try {
-                    const result = await this.unsplash.search.getPhotos({query: animal, per_page: 30})
-                    let images = result.response.results.map((d) => {return {user_name: d.user.name, user_href: d.user.links.html, img_url:d.urls.raw}})
+            try {
+                let query = `${animal} animal`
+                const result = await this.unsplash.search.getPhotos({query: query, per_page: 30})
+                let images = result.response.results.map((d) => {return {id: d.id, alt: image_alt, image_categories: categories, user_name: d.user.name, user_href: d.user.links.html, img_url:d.urls.raw}})
+                if (images.length !== 0){
                     await this.cloudflare.putValue(`${animal}.images`, images, 'data')
-                } catch (e){
-                    objlog("Error loading images", e)
+                }   
+                objlog('images retrieved', images)
+                if (first_run){
+                    return await this.getImages(animal, false)
                 }
+            } catch (e){
+                objlog("Error loading images", e)
             }
         }
-        return images.random()
+        return images
     }
 
 }
